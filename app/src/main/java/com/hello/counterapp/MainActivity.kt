e17f6d43cbd9079b71dfc086637d86b43ccbf52c
Giovana Yokota <gnyokota@gmail.com>
2022-12-02 17:07:57 +0100

    add binding

--- app/src/main/java/com/hello/counterapp/MainActivity.kt
+++ app/src/main/java/com/hello/counterapp/MainActivity.kt
@@ -3,60 +3,59 @@ package com.hello.counterapp
 import android.content.Context
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.hello.counterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var numberText: TextView
-    private lateinit var numberToSubmit: EditText
-    private lateinit var numberToInterval: EditText
-    private lateinit var summaryText: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        val submitButton: Button = findViewById(R.id.main_activity_btn_submit)
-        val randomButton: Button = findViewById(R.id.main_activity_btn_random_number)
-        val incrementButton: Button = findViewById(R.id.main_activity_btn_increment)
-        val decrementButton: Button = findViewById(R.id.main_activity_btn_decrement)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        submitButton.setOnClickListener { submitNumber() }
-        randomButton.setOnClickListener { generateRandomNumber() }
-        incrementButton.setOnClickListener { incrementDecrement(true) }
-        decrementButton.setOnClickListener { incrementDecrement(false) }
+        binding.mainActivityBtnSubmit.setOnClickListener { submitNumber() }
+        binding.mainActivityBtnRandomNumber.setOnClickListener { generateRandomNumber() }
+        binding.mainActivityBtnIncrement.setOnClickListener { incrementDecrement(true) }
+        binding.mainActivityBtnDecrement.setOnClickListener { incrementDecrement(false) }
 
-        numberText = findViewById(R.id.main_activity_tv_number)
-        numberToSubmit = findViewById(R.id.main_activity_et_number_input)
-        numberToInterval = findViewById(R.id.main_activity_et_dec_inc_number)
-        summaryText = findViewById(R.id.main_activity_tv_summary)
+        if(savedInstanceState !== null){
+            binding.mainActivityTvNumber.text = savedInstanceState.getString("textNumber")
+            binding.mainActivityTvSummary.text = savedInstanceState.getString("summary")
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("textNumber", binding.mainActivityTvNumber.text.toString())
+        outState.putString("summary", binding.mainActivityTvSummary.text.toString())
     }
 
     private fun submitNumber() {
-        var startingNumber = numberToSubmit.text.toString()
+        var startingNumber = binding.mainActivityEtNumberInput.text.toString()
         if (startingNumber.isEmpty()) {
             startingNumber = (-100..100).random().toString()
-            numberToSubmit.setText(startingNumber)
+            binding.mainActivityEtNumberInput.setText(startingNumber)
         }
-        numberText.setText(startingNumber)
+        binding.mainActivityTvNumber.setText(startingNumber)
         hideKeyboard()
     }
 
     private fun generateRandomNumber() {
         val randomNumber = (-100..100).random()
-        numberText.setText(randomNumber.toString())
+        binding.mainActivityEtNumberInput.setText(randomNumber.toString())
     }
 
     private fun incrementDecrement(isPositive: Boolean) {
-        val currentNumber = numberText.text.toString().toInt()
-        var incrementValue = numberToInterval.text.toString()
+        val currentNumber = binding.mainActivityTvNumber.text.toString().toInt()
+        var incrementValue = binding.mainActivityEtDecIncNumber.text.toString()
 
         if (incrementValue.isEmpty()) {
             incrementValue = (-100..100).random().toString()
-            numberToInterval.setText(incrementValue)
+            binding.mainActivityEtDecIncNumber.setText(incrementValue)
         }
 
         val calculatedNumber = when (isPositive) {
@@ -68,12 +67,12 @@ class MainActivity : AppCompatActivity() {
             true -> "$currentNumber + ($incrementValue) = $calculatedNumber"
             false -> "$currentNumber - ($incrementValue) = $calculatedNumber"
         }
-        summaryText.setText(message)
+        binding.mainActivityTvSummary.setText(message)
         hideKeyboard()
     }
 
     private fun hideKeyboard() {
         val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputManager.hideSoftInputFromWindow(numberText.windowToken, 0)
+        inputManager.hideSoftInputFromWindow(binding.mainActivityTvNumber.windowToken, 0)
     }
 }
